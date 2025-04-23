66551d9ec784cdc49367e592809fd3ae6f84d9b5
jey1601 <fernandoespinalguevara@gmail.com>
2025-04-22 19:23:04 -0600

    db configurations

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import uvicorn
+import json
 from fastapi import FastAPI
-
+from utils.database import execute_query_json
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"project": "POKE QUEUE REQUEST"}
+    query="SELECT * FROM pokequeue.MESSAGES;"
+    result = await execute_query_json(query)
+    result_dict = json.loads(result)
+
+    return result_dict
 
 @app.get("/api/version")
 async def root():
-    return {"version": "0.0.0"}
+    return {"version": "0.1.0"}
 
 
 
